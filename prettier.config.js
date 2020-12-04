6d33c27f24e1f4fcbe4395122f421a3738d87040
matt <dahv24@gmail.com>
2020-12-04 01:37:40 -0500

    chore(linter): Set up linters, jest and lint-staged

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  singleQuote: true,
+  trailingComma: 'all',
+  allowParens: 'avoid',
+  endOfLine: 'lf',
+  printWidth: 140,
+  tabWidth: 2,
+  useTabs: false,
+  bracketSpacing: true,
+};
